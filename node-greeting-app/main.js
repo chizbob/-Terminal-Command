50ae24e1efb8ca4ca289b5fb73bc525196dddeab
Soo Kim <chizbob@gmail.com>
2019-01-21 15:55:51 -0800

    basic node app completed

---
+++ node-greeting-app/main.js
@@ -0,0 +1,10 @@
+const greeting = require("./greeting.js")
+const myModule = require("./my-module.js")
+
+greeting.greetingEnglish()
+greeting.greetingJapanese()
+greeting.greetingSpanish()
+greeting.greetingIcelandic()
+
+myModule.sum(3, 5)
+myModule.multiplication(4, 5)
---
+++ node-greeting-app/my-module.js
@@ -0,0 +1,7 @@
+exports.sum = function(a, b){
+  console.log(`sum of the two: ${a+b}`)
+}
+
+exports.multiplication = function(a, b){
+  console.log(`multiplication of the two: ${a*b}`)
+}
